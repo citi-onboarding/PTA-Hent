fc89af6c1c547a9b2b2b231b144dc57318937237
hibianca <biancanathally04@gmail.com>
2023-12-15 13:21:14 -0300

    Bloco de Comparative adicionado

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,7 +3,15 @@ import Key from "./key.svg";
 import Paper from "./paper.svg";
 import Library from "./library.svg";
 
+import Check from "./check.svg";
+import Icon from "./icon.svg";
+import XIcon from "./x-icon.svg";
+
 export { Newspaper };
 export { Key };
 export { Paper };
 export { Library };
+
+export { Check };
+export { Icon };
+export { XIcon };
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { HomeContainer } from "./style";
 import Services from "./Services";
+import Comparative from "./Comparative";
 
 export default function Home() {
   return (
     <HomeContainer>
       
       <Services />
+
+      <Comparative />
 
     </HomeContainer>
   );
